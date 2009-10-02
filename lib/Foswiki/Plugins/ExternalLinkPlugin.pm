448f49cce9c03aeb9aeae082000b8193402c2322
ArthurClemens <ArthurClemens@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-10-02 23:21:43 +0000

    Item2199: moved style to a template
    git-svn-id: http://svn.foswiki.org/trunk/ExternalLinkPlugin@5201 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/ExternalLinkPlugin.pm
+++ lib/Foswiki/Plugins/ExternalLinkPlugin.pm
@@ -13,7 +13,7 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at 
+# GNU General Public License for more details, published at
 # http://www.gnu.org/copyleft/gpl.html
 
 package Foswiki::Plugins::ExternalLinkPlugin;
@@ -21,9 +21,8 @@ package Foswiki::Plugins::ExternalLinkPlugin;
 our $VERSION           = '$Rev$';
 our $RELEASE           = '1.20';
 our $NO_PREFS_IN_TOPIC = 1;
-our $SHORTDESCRIPTION =
-  'Adds a visual indicator to outgoing links';
-our $pluginName = 'ExternalLinkPlugin';  # Name of this Plugin
+our $SHORTDESCRIPTION  = 'Adds a visual indicator to outgoing links';
+our $pluginName = 'ExternalLinkPlugin';    # Name of this Plugin
 
 my $protocolsPattern;
 my $addedToHead;
@@ -32,54 +31,59 @@ sub initPlugin {
     my ( $topic, $web, $user, $installWeb ) = @_;
 
     # check for Plugins.pm versions
-    if( $Foswiki::Plugins::VERSION < 1.021 ) {
-        Foswiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
+    if ( $Foswiki::Plugins::VERSION < 1.021 ) {
+        Foswiki::Func::writeWarning(
+            "Version mismatch between $pluginName and Plugins.pm");
         return 0;
     }
 
-    $protocolsPattern = Foswiki::Func::getRegularExpression('linkProtocolPattern');
-	$addedToHead = 0;
-	addStyleToHead();
-	
+    $protocolsPattern =
+      Foswiki::Func::getRegularExpression('linkProtocolPattern');
+    $addedToHead = 0;
+    addStyleToHead();
+
     # Plugin correctly initialized
-    Foswiki::Func::writeDebug( "- Foswiki::Plugins::${pluginName}::initPlugin( $web.$topic ) is OK" ) if $Foswiki::cfg{Plugins}{ExternalLinkPlugin}{Debug};
+    Foswiki::Func::writeDebug(
+        "- Foswiki::Plugins::${pluginName}::initPlugin( $web.$topic ) is OK")
+      if $Foswiki::cfg{Plugins}{ExternalLinkPlugin}{Debug};
     return 1;
 }
 
 sub addStyleToHead {
 
-    return if $addedToHead; 
+    return if $addedToHead;
 
-    Foswiki::Func::writeDebug( "- ${pluginName}::addStyleToHead" ) if $Foswiki::cfg{Plugins}{ExternalLinkPlugin}{Debug};
+    Foswiki::Func::writeDebug("- ${pluginName}::addStyleToHead")
+      if $Foswiki::cfg{Plugins}{ExternalLinkPlugin}{Debug};
 
-    my $style = <<END;
-<style type="text/css" media="all">
-.externalLink a:link,
-.externalLink a:visited,
-.externalLink a:hover {
-	background-image:url($Foswiki::cfg{Plugins}{ExternalLinkPlugin}{MarkerImage});
-	background-repeat:no-repeat;
-	background-position:right;
-	padding-right:18px;
-}
-END
-	Foswiki::Func::addToHEAD($pluginName, $style);
+    # Untaint is required if use locale is on
+    Foswiki::Func::loadTemplate(
+        Foswiki::Sandbox::untaintUnchecked( lc($pluginName) ) );
+    my $header = Foswiki::Func::expandTemplate('externallink:header');
+
+    $header =~
+      s/%markerimage%/$Foswiki::cfg{Plugins}{ExternalLinkPlugin}{MarkerImage}/g;
+    Foswiki::Func::addToHEAD( $pluginName, $header );
+
     $addedToHead = 1;
 }
 
 sub commonTagsHandler {
 ### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
 
-    Foswiki::Func::writeDebug( "- ${pluginName}::commonTagsHandler( $_[2].$_[1] )" ) if $Foswiki::cfg{Plugins}{ExternalLinkPlugin}{Debug};
+    Foswiki::Func::writeDebug(
+        "- ${pluginName}::commonTagsHandler( $_[2].$_[1] )")
+      if $Foswiki::cfg{Plugins}{ExternalLinkPlugin}{Debug};
 
     # This is the place to define customized tags and variables
     # Called by Foswiki::handleCommonTags, after %INCLUDE:"..."%
 
     # do custom extension rule, like for example:
-    $_[0] =~ s!(\[\[($protocolsPattern://[^]]+?)\]\[[^]]+?\]\]([&]nbsp;)?)! handleExternalLink($1, $2) !ge;
+    $_[0] =~
+s!(\[\[($protocolsPattern://[^]]+?)\]\[[^]]+?\]\]([&]nbsp;)?)! handleExternalLink($1, $2) !ge;
 }
 
-sub internalLink          { _internalLink(@_) }
+sub internalLink { _internalLink(@_) }
 
 sub _internalLink {
     my $pre = shift;
@@ -89,16 +93,21 @@ sub _internalLink {
 }
 
 sub handleExternalLink {
-  my ( $wholeLink, $url ) = @_;
+    my ( $wholeLink, $url ) = @_;
 
-  my $scriptUrl = Foswiki::Func::getUrlHost() . Foswiki::Func::getScriptUrlPath();
-  my $pubUrl = Foswiki::Func::getUrlHost() . Foswiki::Func::getPubUrlPath();
+    my $scriptUrl =
+      Foswiki::Func::getUrlHost() . Foswiki::Func::getScriptUrlPath();
+    my $pubUrl = Foswiki::Func::getUrlHost() . Foswiki::Func::getPubUrlPath();
 
-  if (($url =~ /^$scriptUrl/) || ($url =~ /^$pubUrl/ ) || ($wholeLink =~ /[&]nbsp;$/)) {
-    return $wholeLink;
-  } else {
-    return "<span class='externalLink'>$wholeLink</span>";
-  }
+    if (   ( $url =~ /^$scriptUrl/ )
+        || ( $url =~ /^$pubUrl/ )
+        || ( $wholeLink =~ /[&]nbsp;$/ ) )
+    {
+        return $wholeLink;
+    }
+    else {
+        return "<span class='externalLink'>$wholeLink</span>";
+    }
 
 }
 
